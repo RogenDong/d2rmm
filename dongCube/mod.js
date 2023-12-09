8ae6322cb68c5af19eb5b4763869af41fc0b8a1a
dongRogen <3601778801@qq.com>
2023-12-09 18:34:58 +0800

    feat (cube) 新增配方 - 新配方：骨石+紫药合成 头颅盾 - 简化代码 - 改孔配方需要消耗完美钻石

--- dongCube/mod.js
+++ dongCube/mod.js
@@ -1,30 +1,43 @@
-
-function reSocketRecipe(ss) {
-  return {
+const path_cube = 'global\\excel\\cubemain.txt';
+const tsv_cube = D2RMM.readTsv(path_cube);
+for (let ss = 1; ss <= 6; ss++) {
+  const base = {
     enabled: 1,
     version: 100,
-    numinputs: ss + 1,
+    numinputs: ss + 2,
     // input 1 defined below
-    'input 2': `"${config.reSocketItem2},qty=${ss}"`,
+    'input 2': 'gpw',
+    'input 3': `"${config.reSocketItem},qty=${ss}"`,
     output: 'useitem',
     '*eol': 0,
     'mod 1': 'sock',
     'mod 1 min': ss,
     'mod 1 max': ss,
   };
-}
-const path_cubemain = 'global\\excel\\cubemain.txt';
-const tsv_cubemain = D2RMM.readTsv(path_cubemain);
-for (let ss = 1; ss <= 6; ss++) {
-  tsv_cubemain.rows.push({
-    ...(reSocketRecipe(ss)),
+  tsv_cube.rows.push({
+    ...base,
     description: 'Change Sockets On Weapon',
     'input 1': '"weap,sock"',
   });
-  tsv_cubemain.rows.push({
-    ...(reSocketRecipe(ss)),
+  tsv_cube.rows.push({
+    ...base,
     description: 'Change Sockets On  Armor',
     'input 1': '"armo,sock"',
   });
 }
-D2RMM.writeTsv(path_cubemain, tsv_cubemain);
+// 骷髅石 + 活力药剂合成死灵盾
+const inp_skull = ['skc', 'skf', 'sku', 'skl', 'skz'];
+for (let x = 0; x < inp_skull.length; x++) {
+  const ss = inp_skull[x];
+  tsv_cube.rows.push({
+    description: 'Making specimens ' + ss,
+    enabled: 1,
+    version: 100,
+    numinputs: 2,
+    'input 1': ss,
+    'input 2': 'rvs',
+    output: 'ne' + (x + 1),
+    '*eol': 0,
+  });
+}
+D2RMM.writeTsv(path_cube, tsv_cube);
