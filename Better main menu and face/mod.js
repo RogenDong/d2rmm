9d855a808cb9a2b3570ed26749ed97e0b63d7ab0
dongRogen <3601778801@qq.com>
2023-12-11 02:07:28 +0800

    feat (better main)

---
+++ Better main menu and face/mod.js
@@ -0,0 +1,10 @@
+D2RMM.copyFile(
+  'hd', // <mod folder>\hd
+  'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+  true // overwrite any conflicts
+);
+D2RMM.copyFile(
+  'global', // <mod folder>\global
+  'global', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\global
+  true // overwrite any conflicts
+);
