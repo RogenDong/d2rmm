de9f567294e062fc7372c5b30cdbfdde127f20b8
dongRogen <3601778801@qq.com>
2023-12-22 01:07:36 +0800

    feat (socket-punching) 添加视频打孔配方

--- SocketPunching/mod.js
+++ SocketPunching/mod.js
@@ -1,56 +1,66 @@
 const cubemainFilename = 'global\\excel\\cubemain.txt';
 const cubemain = D2RMM.readTsv(cubemainFilename);
-if (config.unsocket) {
-  function unsocketRecipe() {
-    return {
-      enabled: 1,
-      version: 100,
-      numinputs: 2,
-      // input 1 defined below
-      'input 2': config.item,
-      output: '"useitem,rem"',
-      '*eol': 0,
-    };
-  }
 
+// add unsocket recipe
+if (config.unsocket) {
+  const tpl_unsocket = {
+    enabled: 1,
+    version: 100,
+    numinputs: 2,
+    // input 1 defined below
+    'input 2': config.item,
+    output: '"useitem,rem"',
+    '*eol': 0,
+  };
   cubemain.rows.push({
-    ...(unsocketRecipe()),
+    ...tpl_unsocket,
     description: 'Empty Sockets On  Armor',
     'input 1': '"armo,sock"',
   });
 
   cubemain.rows.push({
-    ...(unsocketRecipe()),
+    ...tpl_unsocket,
     description: 'Empty Sockets On Weapon',
     'input 1': '"weap,sock"',
   });
 }
 
-function socketRecipe(sockets) {
-  return {
-    enabled: 1,
-    version: 100,
-    numinputs: sockets + 1,
-    // input 1 defined below
-    'input 2': `"${config.item},qty=${sockets}"`,
-    output: 'useitem',
-    'mod 1': 'sock',
-    'mod 1 min': sockets,
-    'mod 1 max': sockets,
-    '*eol': 0,
-  };
-}
-for (let sockets = 1; sockets <= 6; sockets++) {
-  if (config.socket) {
+// add socket recipe
+if (config.socket) {
+  for (let s = 1; s <= 6; s++) {
+    const tpl_socket = {
+      enabled: 1,
+      version: 100,
+      numinputs: s + 1,
+      // input 1 defined below
+      'input 2': `"${config.item},qty=${s}"`,
+      output: 'useitem',
+      'mod 1': 'sock',
+      'mod 1 min': s,
+      'mod 1 max': s,
+      '*eol': 0,
+    };
     cubemain.rows.push({
-      ...(socketRecipe(sockets)),
-      description: `Add ${sockets} Sockets To  Armor`,
+      ...tpl_socket,
+      description: `Add ${s} Sockets To  Armor`,
       'input 1': '"armo,nos"',
     });
     cubemain.rows.push({
-      ...(socketRecipe(sockets)),
-      description: `Add ${sockets} Sockets To Weapon`,
+      ...tpl_socket,
+      description: `Add ${s} Sockets To Weapon`,
       'input 1': '"weap,nos"',
+    });
+
+    if (s > 1) continue;
+    cubemain.rows.push({
+      ...tpl_socket,
+      description: `Add Sockets To Ring`,
+      'input 1': 'ring',
+    });
+    cubemain.rows.push({
+      ...tpl_socket,
+      description: `Add Sockets To Amulet`,
+      'input 1': 'amul',
     });
   }
 }
