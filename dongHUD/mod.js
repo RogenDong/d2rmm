67b1830a3ed401ec104b7c17d2b213b575005aca
dongRogen <3601778801@qq.com>
2024-01-09 20:51:31 +0800

    feat HUD 新增能见度、UI相关调整 - 扩大可视范围 - 有箭头指明地图中的传送点和各种门

---
+++ dongHUD/mod.js
@@ -0,0 +1,7 @@
+// hd\character\player\*          去除迷雾
+// hd\[objects, roomtiles, vfx]   箭头指向传送点、各种门
+D2RMM.copyFile(
+  'hd', // <mod folder>\hd
+  'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+  true // overwrite any conflicts
+);
