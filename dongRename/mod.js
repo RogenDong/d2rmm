bc56454ed3228f20393270230e46165e4930ac60
dongRogen <3601778801@qq.com>
2024-01-10 16:03:00 +0800

    feat (rename) 新增大菠萝6阶段文本提示 pref 优化装备品质相关代码 pref 补充注释 pref 去掉宝石染色代码

--- dongRename/mod.js
+++ dongRename/mod.js
@@ -1,26 +1,19 @@
 /** 防具重量 */
 const A_SPEED = {
-  /** 轻 */
-  0: '',
-  /** 中 */
-  5: '',
-  /** 重 */
-  10: '',
+  0: '',// 轻
+  5: '',// 中
+  10: '',// 重
 };
 /** 防具类型 */
 const A_TYPE = {
   normcode: '',
-  /** 扩展 */
-  ubercode: '',
-  /** 精华 */
-  ultracode: '',
+  ubercode: '',// 扩展
+  ultracode: '',// 精华
 };
-/** 装备类型 */
+/** 武器类型 */
 const W_TYPE = {
   normcode: '',
-  /** 扩展 */
   ubercode: '',
-  /** 精华 */
   ultracode: '',
 };
 /** 物等表 */
@@ -34,6 +27,7 @@ const DICT = {
   ubercode: '扩',
   ultracode: 'ÿcL精ÿc0',
 };
+
 /** 地图等级 */
 const LEVEL_LV = {
   788: "[28/64/81]",
@@ -157,25 +151,37 @@ const LEVEL_LV = {
 
 /** 找出防具【普、扩、精】【轻、中、重】 */
 function map_armor() {
+  // 读取装备表
   const ls_armor = D2RMM.readTsv('global\\excel\\armor.txt');
   for (const a of ls_armor.rows) {
+    // 记录装备物等，后面要用
     LVL[a.code] = a.level;
     let pass = true;
+    // 每一种装备的 code 是唯一的；但一系装备的【普 扩 精】数据是相同的
+    // 将 code 与 normcode，ubercode，ultracode 分别比较，找到这种装备属于哪个品质
+    // normcode 普通品质的 code
+    // ubercode 扩展
+    // ultracode 精华
     for (const col in A_TYPE) {
       if (a.code === a[col]) {
+        // 记录下 code 一会用来搜索装备名称
         A_TYPE[col] += a.code + ' ';
         pass = false;
         break;
       }
     }
+    // 没找到品质就不用继续检查重量了，看下一件装备
     if (pass) continue;
+    // speed 枚举了3个数字代表【轻 中 重】
+    // 0=轻  5=中  10=重
     for (let s = 0; s <= 10; s += 5) {
       if (a.speed == s) {
+        // 记录下 code 一会用来搜索装备名称
         A_SPEED[s] += a.code + ' ';
         break;
       }
-    }
-  }
+    }// end loop: speed
+  }// end loop: all armors
 }
 /** 找出武器【普、扩、精】 */
 function map_weapon() {
@@ -230,9 +236,9 @@ function rename_affixe() {
       nam.zhTW = 'G';
     }
     // 骷髅,宝石词缀修改
-    else if ((/^gs[bgrw]$/g).test(nam.Key)) {
-      nam.zhTW = `ÿc1${nam.zhCN}ÿc0`;
-    }
+    // else if ((/^gs[bgrw]$/g).test(nam.Key)) {
+    //   nam.zhTW = `ÿc1${nam.zhCN}ÿc0`;
+    // }
     else {
       // nam.zhTW = nam.zhCN + "_" + nam.enUS;
       nam.zhTW = nam.zhCN;
@@ -241,35 +247,41 @@ function rename_affixe() {
   D2RMM.writeJson(path_affixe, ls_affixe);
 }
 
-// 获取品质、轻重
-function get_type_sfx(name) {
-  let sfx = LVL[name.Key];
-  if (sfx == null) {
-    sfx = '';
+/** 获取品质、轻重 */
+function get_comment(name) {
+  // 物等
+  let lv = LVL[name.Key];
+  if (lv == null) {
+    return null;
   }
-  // 武器
+  // 看是不是武器
   for (const t in W_TYPE) {
     if (W_TYPE[t].includes(name.Key)) {
-      return DICT[t] + sfx;
+      // 确定是武器，后面的检查是防具的，跳过
+      return DICT[t] + lv;
     }
   }
-  let tmp = null;
-  // 防具轻重
-  for (const s in A_SPEED) {
-    if (A_SPEED[s].includes(name.Key)) {
-      tmp = DICT[s];
+
+  // 看是不是防具
+  let type = '';// 防具品质
+  let weight = '';// 防具轻重
+
+  // 找防具品质
+  for (const t in A_TYPE) {
+    if (A_TYPE[t].includes(name.Key)) {
+      type = DICT[t];
       break;
     }
   }
-  // 防具品质
-  if (tmp != null) {
-    for (const t in A_TYPE) {
-      if (A_TYPE[t].includes(name.Key)) {
-        return DICT[t] + tmp + sfx;
-      }
+  // 找防具轻重
+  for (const s in A_SPEED) {
+    if (A_SPEED[s].includes(name.Key)) {
+      weight = DICT[s];
+      break;
     }
   }
-  return sfx;
+
+  return type + weight + lv;
 }
 
 /** 重命名物品 */
@@ -336,7 +348,7 @@ function rename_item() {
     }
     // 装备品质 + 轻重
     if (W_TYPE.normcode.length > 0 || A_TYPE.normcode.length > 0) {
-      const sfx = get_type_sfx(nam);
+      const sfx = get_comment(nam);
       if (sfx != null) {
         nam.zhTW = `${nam.zhTW} ${sfx}`;
       }
@@ -378,6 +390,11 @@ function recolor_ui() {
     // ItemNameMagicFormatPrefixOnly
     else if (ui.id == 26775) {
       ui.zhTW = 'p%0 %1';
+    }
+    // 大菠萝 6阶段文本
+    else if (ui.id >= 27186 && ui.id <= 27191) {
+      let step = ui.id - 27185;
+      ui.zhTW = `进入阶段：${step}/6`;
     }
   }
   D2RMM.writeJson(path_str_ui, ls_sui);
