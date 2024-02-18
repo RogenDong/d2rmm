3428110d624d628233073d13f2e5436566560902
dongRogen <3601778801@qq.com>
2024-02-19 00:00:42 +0800

    todo HUD 调整血条

--- dongHUD/mod.js
+++ dongHUD/mod.js
@@ -1,7 +1,14 @@
-// hd\character\player\*          去除迷雾
-// hd\[objects, roomtiles, vfx]   箭头指向传送点、各种门
+// hd\character\player\*                      去除迷雾
+// hd\[objects, roomtiles, vfx]               箭头指向传送点、各种门
+// hd\global\ui\panel\hud_02\monsterhealth\*  修改血条
 D2RMM.copyFile(
   'hd', // <mod folder>\hd
   'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
   true // overwrite any conflicts
 );
+// global\ui\layouts\*  修改血条
+//D2RMM.copyFile(
+//  'hd', // <mod folder>\hd
+//  'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+//  true // overwrite any conflicts
+//);
